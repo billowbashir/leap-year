cd443abad4b69a82856ea6fd5a9c63acffe760bf
billowbashir <billowbashir@gmail.com>
2018-07-11 18:05:03 +0300

    our fourth spec has passed

--- js/scripts.js
+++ js/scripts.js
@@ -18,7 +18,7 @@ var leapYear=function(year) {
   }
 };
 var leapYear =function(year) {
-  if ((year%4===0)&&(year%100 !==0)){
+  if ((year%4===0)&&(year%100 !==0) || (year%400===0)){
     return true;
   } else {
     return false;
