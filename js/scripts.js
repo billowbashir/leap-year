58910cc26709a0e434749e10dc7ee9b2b16179a8
billowbashir <billowbashir@gmail.com>
2018-07-11 17:36:47 +0300

    our second spec passed

--- js/scripts.js
+++ js/scripts.js
@@ -9,3 +9,11 @@ $(document).ready(function(){
 var leapYear=function(year) {
   return false;
 };
+var leapYear=function(year) {
+  if (year%4==0) {
+  return true;
+}
+  else {
+    return false;
+  }
+};
