195945e118a566858144f2274c9867ee6aa5633f
billowbashir <billowbashir@gmail.com>
2018-07-11 17:59:45 +0300

    refractor our code

--- js/scripts.js
+++ js/scripts.js
@@ -18,11 +18,9 @@ var leapYear=function(year) {
   }
 };
 var leapYear =function(year) {
-  if (year%100===0) {
+  if ((year%4===0)&&(year%100 !==0)){
+    return true;
+  } else {
     return false;
-  } else if (year%4===0) {
-  return true;
-} else {
-  return false;
-}
+  }
 };
