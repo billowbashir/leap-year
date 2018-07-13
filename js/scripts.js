865356634f0a36da85c1e220920faf0aa3c962c6
billowbashir <billowbashir@gmail.com>
2018-07-13 10:40:08 +0300

    change the output from boolean to string

--- js/scripts.js
+++ js/scripts.js
@@ -3,20 +3,15 @@ $(document).ready(function(){
     event.preventDefault();
     var year=parseInt($("input#year").val());
     var result=leapYear(year);
-    $("#result").text(result);
+    $(".year").text(year);
+    if (result===false){
+      $(".not").text("not");
+    } else {
+      $(".not").text(" ");
+    }
+    $("#result").show();
   });
 });
-var leapYear=function(year) {
-  return false;
-};
-var leapYear=function(year) {
-  if (year%4===0) {
-  return true;
-}
-  else {
-    return false;
-  }
-};
 var leapYear =function(year) {
   if ((year%4===0)&&(year%100 !==0) || (year%400===0)){
     return true;
